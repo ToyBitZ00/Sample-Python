5f37dd5af86f98302a92897cd032912a19cc629d
ToyBitZ00 <pa6308191@gmail.com>
2025-09-09 09:55:18 +0800

    Added age, car and student user input.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-
-
 first_name = input("Your First Name: ")
 last_name = input("Your Last Name: ")
 
-full_name = {f"{first_name} {last_name}"}
+full_name = f"{first_name} {last_name}"
+
+age = input("Your Age: ")
+car = input("Your Car: ")
+
+student = bool(input("Are you a student [Y/N]: "))
+
 
 print(full_name)
+print(age)
+print(car)
+print(student)
