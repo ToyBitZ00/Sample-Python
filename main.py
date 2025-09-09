c305c05f47684a64e5ebd058beeb5e569fb2c1aa
ToyBitZ00 <pa6308191@gmail.com>
2025-09-09 10:06:39 +0800

    Added if statements that check if a person is a student, if true, then the person will be asked about his school, if not a student, he will not be asked.

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ full_name = f"{first_name} {last_name}"
 age = input("Your Age: ")
 car = input("Your Car: ")
 
-student = bool(input("Are you a student [Y/N]: "))
+student = bool(input("Are you a student [true / false]: "))
 
+if (student == "True"):
+    school = input("What school are you from: ")
+else:
+    print("\n")
+    print(full_name)
+    print(age, "yrs old")
+    print(car)
 
-print(full_name)
-print(age)
-print(car)
-print(student)
+if (student == "True"):
+    print("You are a student")
+    print("You are studying in: ", school)
+else:
+    print("You are not a student")
